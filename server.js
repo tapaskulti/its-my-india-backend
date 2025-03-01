9727672cfc769f5dd7b2c4e79267919120b65773
Tapas Chandra <tapaskulti@gmail.com>
2025-03-02 02:22:50 +0530

    server code changed

--- server.js
+++ server.js
@@ -8,31 +8,53 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   cors({
+//     // origin: ALLOWED_ORIGINS,
+//     origin: function (origin, callback) {
+//       // allow requests with no origin
+//       // (like mobile apps or curl requests)
+//       if (!origin) return callback(null, true);
+//       if (
+//         // [process.env.FRONT_END1, process.env.FRONT_END2].indexOf(origin) === -1
+//         ["https://my-india-frontend-3nlyv.ondigitalocean.app"].indexOf(origin) === -1
+
+//       ) {
+//         console.log(origin);
+//         // console.log(ALLOWED_ORIGINS.indexOf(origin));
+//         var msg =
+//           'The CORS policy for this site does not ' +
+//           'allow access from the specified Origin.';
+//         return callback(new Error(msg), false);
+//       }
+//       return callback(null, true);
+//     },
+//     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'], // Methods you want to allow
+//     allowedHeaders: ['Content-Type', 'Authorization'], // Specify headers you want to allow
+//     exposedHeaders: ['Set-Cookie'],
+//     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+//   })
+// );
+
+const allowedDomains = ["https://my-india-frontend-3nlyv.ondigitalocean.app"];
 app.use(
   cors({
-    // origin: ALLOWED_ORIGINS,
     origin: function (origin, callback) {
       // allow requests with no origin
       // (like mobile apps or curl requests)
       if (!origin) return callback(null, true);
-      if (
-        // [process.env.FRONT_END1, process.env.FRONT_END2].indexOf(origin) === -1
-        ["https://my-india-frontend-3nlyv.ondigitalocean.app"].indexOf(origin) === -1
-
-      ) {
-        console.log(origin);
-        // console.log(ALLOWED_ORIGINS.indexOf(origin));
+      if (allowedDomains.indexOf(origin) === -1) {
         var msg =
-          'The CORS policy for this site does not ' +
-          'allow access from the specified Origin.';
+          "The CORS policy for this site does not " +
+          "allow access from the specified Origin.";
         return callback(new Error(msg), false);
       }
       return callback(null, true);
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'], // Methods you want to allow
-    allowedHeaders: ['Content-Type', 'Authorization'], // Specify headers you want to allow
-    exposedHeaders: ['Set-Cookie'],
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+    },    
+    credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    optionsSuccessStatus: 200,
   })
 );
 
